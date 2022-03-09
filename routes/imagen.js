50befa146507fdb9d27d6c5a2f0748272341a133
Jorge Bover <jorgebover@gmail.com>
2022-03-08 22:35:35 -0300

    tarea módulo 4 unidad 3

---
+++ routes/imagen.js
@@ -0,0 +1,10 @@
+const src = require('debug');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('imagen');
+});
+
+module.exports = router;
